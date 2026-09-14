a33bb1b2c030fdc28f3d4bb14e9b57c520ca5330
developeroutsetstudio <developer0001112@gmail.com>
2026-09-14 15:59:32 +0530

    feat: update testimonials section image to image34

--- src/lib/data.js
+++ src/lib/data.js
@@ -248,7 +248,7 @@ export const testimonialsData = {
       quote:
         "Outset Studio delivers thoughtful design, seamless execution, and strategic growth solutions that transform spaces into memorable, customer-focused experiences.",
       avatar: "/image14.jpg",
-      image: "/image10.jpg",
+      image: "/image34.png",
     },
     {
       id: 2,
@@ -258,7 +258,7 @@ export const testimonialsData = {
       quote:
         "Working with Outset Studio was a game changer for our outlet launch. Their attention to detail and commercial focus set them apart from traditional studios.",
       avatar: "/image14.jpg",
-      image: "/image11.jpg",
+      image: "/image34.png",
     },
   ],
 };
