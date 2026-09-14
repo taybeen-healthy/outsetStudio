f70f07cd2560ac2aa99e2cd76e01d6d7277f8022
developeroutsetstudio <developer0001112@gmail.com>
2026-09-14 16:24:47 +0530

    fix: update stats label to TOTAL NUMBER OF PROJECTS

--- src/lib/data.js
+++ src/lib/data.js
@@ -30,7 +30,7 @@ export const heroData = {
 export const statsData = [
   { id: 1, value: "10+", label: "TOTAL VENDORS", highlight: true },
   { id: 2, value: "10+", label: "EXPERIENCED VENDORS", highlight: false },
-  { id: 3, value: "15+", label: "SPECIALIZED CATEGORIES", highlight: false },
+  { id: 3, value: "15+", label: "TOTAL NUMBER OF PROJECTS", highlight: false },
   { id: 4, value: "07+", label: "YEARS OF EXPERIENCE", highlight: false },
 ];
 
