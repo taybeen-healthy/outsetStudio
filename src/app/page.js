bd8a1f78bd7aa26f230fac1a0a38b4270bfb6538
developeroutsetstudio <developer0001112@gmail.com>
2026-09-17 14:43:01 +0530

    chore: add about page images

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ import {
 
 export default function Home() {
   return (
-      <div className="relative w-full bg-[#FAF7F2] lg:bg-white overflow-x-hidden font-sans select-none">
-        <div className="relative lg:min-h-screen flex flex-col justify-between bg-[#FAF7F2] lg:bg-transparent overflow-hidden">
+      <div className="relative w-full bg-[#D6C2BD] lg:bg-white overflow-x-hidden font-sans select-none">
+        <div className="relative lg:min-h-screen flex flex-col justify-between bg-[#D6C2BD] lg:bg-transparent overflow-hidden">
         <div className="hidden lg:block absolute inset-0 z-0 overflow-hidden pointer-events-none">
           <Image
             src={heroData?.backgroundImage ?? "/image1.jpg"}
