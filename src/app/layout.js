199a155a778ecca2aff501a8769ea34f70d3d0b4
developeroutsetstudio <developer0001112@gmail.com>
2026-09-21 17:21:53 +0530

    Remove canonical URL from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -80,9 +80,6 @@ export const metadata = {
     ],
     apple: "/favicon.png",
   },
-  alternates: {
-    canonical: "https://outsetstudio.in",
-  },
 };
 
 export default function RootLayout({ children }) {
